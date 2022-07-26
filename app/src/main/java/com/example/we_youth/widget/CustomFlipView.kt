aecf1358bde3bbe8a036216201442fbad3c2bfe3
liurui <303996789@qq.com>
2022-07-26 15:58:54 +0800

    简单折叠效果

--- app/src/main/java/com/example/we_youth/widget/CustomFlipView.kt
+++ app/src/main/java/com/example/we_youth/widget/CustomFlipView.kt
@@ -1,13 +1,10 @@
 package com.example.we_youth.widget
 
 import android.content.Context
-import android.content.res.Resources
 import android.graphics.*
 import android.os.Build
 import android.util.AttributeSet
 import android.view.View
-import androidx.core.content.ContextCompat
-import androidx.core.graphics.drawable.toBitmap
 import androidx.core.graphics.withSave
 import com.blankj.utilcode.util.Utils
 import com.example.we_youth.R
@@ -23,25 +20,41 @@ class CustomFlipView : View {
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
     private val bitmapWidth = ConvertUtils.dp2px(200f)
-    private val bitmapPadding = ConvertUtils.dp2px(50f).toFloat()
+    private val bitmapPadding = ConvertUtils.dp2px(80f).toFloat()
     private val bitmap = getBitmap(bitmapWidth)
     private val paint = Paint()
     private var camera = Camera()
+    private var bottomRectF = RectF()
+    private var topRectF = RectF()
 
     init {
-//        camera.setLocation(0f, 0f, -5 * resources.displayMetrics.density)
+        camera.setLocation(0f, 0f, -5 * resources.displayMetrics.density)
         // 需要对canvas应用这个旋转效果
         camera.rotateX(30f)
     }
 
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        bottomRectF.set(-bitmapWidth.toFloat() / 2, 0f, bitmapWidth.toFloat() / 2, bitmapWidth / 2f)
+        topRectF.set(-bitmapWidth.toFloat() / 2, -bitmapWidth.toFloat() / 2, bitmapWidth.toFloat() / 2, 0f)
+    }
+
     override fun onDraw(canvas: Canvas) {
-
+        // 上半部分
         canvas.withSave {
-
+            canvas.translate(bitmapPadding + bitmapWidth / 2, bitmapPadding + bitmapWidth / 2)
+//            camera.applyToCanvas(canvas)
+            // 进行剪裁 保留上半部分
+            canvas.clipRect(topRectF)
+            canvas.translate(-(bitmapPadding + bitmapWidth / 2), -(bitmapPadding + bitmapWidth / 2))
+            canvas.drawBitmap(bitmap, bitmapPadding, bitmapPadding, paint)
         }
+
+        // 下半部分
         var count = canvas.save()
         canvas.translate(bitmapPadding + bitmapWidth / 2, bitmapPadding + bitmapWidth / 2)
         camera.applyToCanvas(canvas)
+        // 进行剪裁 保留下半部分
+        canvas.clipRect(bottomRectF)
         canvas.translate(-(bitmapPadding + bitmapWidth / 2), -(bitmapPadding + bitmapWidth / 2))
         canvas.drawBitmap(bitmap, bitmapPadding, bitmapPadding, paint)
         canvas.restoreToCount(count)
@@ -51,7 +64,6 @@ class CustomFlipView : View {
     fun getBitmap(width: Int): Bitmap {
         val options = BitmapFactory.Options()
         options.inJustDecodeBounds = true
-        // 很奇怪 这里传入 R.mipmap.ic_launcher 会解码失败 是因为webp的原因吗？
         BitmapFactory.decodeResource(Utils.getApp().resources, R.drawable.ic_dlam, options)
         options.inJustDecodeBounds = false
         options.inDensity = options.outWidth
